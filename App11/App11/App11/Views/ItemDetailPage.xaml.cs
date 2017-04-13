ea82d112c169590cfe84519d14291f1e5dc3f59f
hw <heatherwensler@gmail.com>
2017-04-12 22:32:30 -0700

    Added items and items detail view models.

--- App11/App11/App11/Views/ItemDetailPage.xaml.cs
+++ App11/App11/App11/Views/ItemDetailPage.xaml.cs
@@ -1,7 +1,14 @@
 ﻿
-using App11.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App11.ViewModels;
+
 
 namespace App11.Views
 {
