9062dfbfb4a4635a24106f116bd06ccef7d6501d
hw <hwensler@users.noreply.github.com>
2017-04-18 17:34:05 -0700

    Created a PCL demo file to attempt to use sqlite

---
+++ PCLDemo/PCLDemo/PCLDemo/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace PCLDemo.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
