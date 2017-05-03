63954602a9e77198e35db2c30cb8d5b88292887c
hw <heatherwensler@gmail.com>
2017-05-02 23:20:47 -0700

    made new view - item create page

---
+++ wenslerh/wenslerh/Views/ItemCreatePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace wenslerh.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemCreatePage : ContentPage
+	{
+		public ItemCreatePage ()
+		{
+			InitializeComponent ();
+
+            Title = "Create an Item";
+
+            var name = new Entry();
+            name.SetBinding(Entry.TextProperty, "Name");
+
+            var description = new Entry();
+            description.SetBinding(Entry.TextProperty, "Name");
+
+            var strength = new Entry();
+            name.SetBinding(Entry.TextProperty, "Strength");
+        }
+	}
+}
