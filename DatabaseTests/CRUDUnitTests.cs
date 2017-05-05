a0f8cea6a8f690de070cd280bfd35d02d0100adb
hw <heatherwensler@gmail.com>
2017-05-04 22:04:09 -0700

    created unit tests to test insert

--- DatabaseTests/CRUDUnitTests.cs
+++ DatabaseTests/CRUDUnitTests.cs
@@ -4,19 +4,63 @@ using wenslerh.Models;
 using wenslerh.Services;
 using wenslerh;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using wenslerh.Models;
+using System;
+using System.Linq;
 
 namespace DatabaseTests
 {
     [TestClass]
     public class CRUDUnitTests
     {
+
+        //create database to use in further testing
+        //set up a new database
+        ItemsDatabase testDatabase = new ItemsDatabase(Path.GetTempFileName());
+
+        //tests that a database is created
         [TestMethod]
         public void CreateDatabase()
         {
             //set up a new database
-            var testDatabase = new ItemsDatabase(Path.GetTempFileName());
-            Assert.IsNotNull(testDatabase, "The database should not be null after creation. ");
+            var createDatabase = new ItemsDatabase(Path.GetTempFileName());
+            Assert.IsNotNull(createDatabase, "The database should not be null after creation. ");
 
         }
+
+        //tests that an item doesn't return it exists if it doesn't exist
+        [TestMethod]
+        public void DoesItemExistInDatabase()
+        {
+            Task<int> result = testDatabase.DoesItemExist("1");
+            Assert.AreNotEqual(1, result, "The item hasn't been inserted and should not exist");
+
+        }
+
+        //tests insert by showing an item does exist after it is inserted
+        [TestMethod]
+        public async Task InsertIntoDatabaseAsync()
+        {
+            int firstCount = (testDatabase.CountRows()).Result;
+
+            //define an item
+            Item testItem = new Item { ID = Guid.NewGuid().ToString(), Name = "Sword", Description = "A really cool sword. ", Strength = 1 };
+
+            //insert it
+            await testDatabase.SaveItemAsync(testItem);
+
+            int secondCount = (testDatabase.CountRows()).Result;
+
+            Assert.AreEqual(secondCount, firstCount + 1, "The total number of rows before and insert" +
+                "should be one less than the total number of rows after an insert");
+
+
+        }
+
+
+
     }
 }
