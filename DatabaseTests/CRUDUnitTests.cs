324711332c5c7b1b11d95f55880ca168454a9818
hw <heatherwensler@gmail.com>
2017-05-04 20:20:52 -0700

    added create database test

---
+++ DatabaseTests/CRUDUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wenslerh.Models;
+using wenslerh.Services;
+using wenslerh;
+using System.IO;
+
+namespace DatabaseTests
+{
+    [TestClass]
+    public class CRUDUnitTests
+    {
+        [TestMethod]
+        public void CreateDatabase()
+        {
+            //set up a new database
+            var testDatabase = new ItemsDatabase(Path.GetTempFileName());
+            Assert.IsNotNull(testDatabase);
+        }
+    }
+}
